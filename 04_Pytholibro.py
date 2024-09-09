a2ef41730cf2fc4d4d0ed6810cbc540ad4f4f504
DiegoSaquil <dsaquilg@miumg.edu.gt>
2024-09-08 20:15:57 -0600

    REPASOS DE PYTHON

---
+++ 04_Pytholibro.py
@@ -0,0 +1,114 @@
+'''print("1 2 3 4 5")
+print("1,2,3,4,5",sep=',')
+print(1,2,3,4,5, sep=',', end='.') 
+print("Hola, ¿cómo te llamas?")
+nombre = input()
+print("Nos alegramos mucho de que hayas elegido Python" , nombre)
+
+edad = int(input("¿Cuántos años tienes?: "))
+print ("tienes la edad de:", edad)'''
+
+#num1 = int(input("Introduzca el primer sumando: "))
+#num2 = int(input("introduzca el segundo sumando: "))
+#print ("El resultado es: ", num1+num2)
+
+'''numero1 = float(input("Introduzca el primer sumando: "))
+numero2 = float(input("introduzca el segundo sumando: "))
+print ("la suma es de: ", numero1 + numero2)'''
+
+#minuedo = int(input("ingrese el minuendo: "))
+#sustraendo = int(input("ingrese el sustraendo: "))
+#print("el resultado es de: ", minuedo-sustraendo)
+
+
+'''minuendo1 = float(input("Ingrese el minuendo decimal: "))
+sustraendo1 = float(input("Ingrese el sustraendo decimal: "))
+print("el resultado es de: ", minuendo1-sustraendo1)'''
+
+#n1 = int(input("ingrese su primer numero: "))
+#n2 = int(input("ingrese su segundo numero: "))
+#print("el resultado es de: ", n1*n2)
+
+'''n1 = float(input("ingrese su primer numero decimal: "))
+n2 = float(input("ingrese su segundo numero decimal: "))
+print("El resultado es de: ", n1*n2)'''
+
+#dividendo = int(input("ingrese su dividendo: "))
+#divisor = int(input("ingrese su divisor: "))
+#print("el resultado es: ", dividendo/divisor)
+
+'''dividendo = float(input("ingrese su dividendo en decimal: "))
+divisor = float(input("Ingrese su divisor en decimal: "))
+print("el resultado es: ", dividendo/divisor)'''
+
+#dividendo = float(input("introduzca el dividendo: "))
+#divisor = float(input("introduzca el divisor: "))
+#resultado = round(dividendo/divisor, 2) #despues de la ejecución de la operacion determino el numero de decimales
+
+#print("El resultado es de: ", resultado)
+
+'''cadenaejemplo = 'Hola Time of software'
+print(cadenaejemplo)'''
+
+#cadenaejemplo="Hola time of software\nEsto es una cadena\nmultilinea"
+#print(cadenaejemplo)
+
+'''lista = ["ordenador", "teclado", "monitor", "ratón"]
+print (lista)'''
+
+#lista = ["ordenador", "teclado", "ratón"]
+#print(len(lista))  #"len es para saber el numero de elementos de una lista"
+#print(lista[0])
+#print(lista[1])
+#print(lista[2])
+
+'''listao = ["ordenador", 'teclado', 'raton']
+listan = ['monitor', 'impresora', 'altavoces']
+listaf = listao+listan
+print(listaf)
+'''
+#lista = ["ordenador",'teclado','raton']
+#print(lista)
+#lista[0] = 'monitor'
+#lista[1] = 'impresora'
+#lista[2] = 'altavoces'
+#print(lista)
+
+#lista = ["ordenador", 'teclado', "raton"]
+#print(lista)
+#del lista[1] #la instrucción del elimina el dato de la lista que elijamos dependiendo del número que pongamos en la lista
+#print (lista)
+
+'''
+lista = ["ordenador","teclado","raton",["tarjeta de sonido","microfono","altavoces"]]
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
+print(lista[3][0])
+print(lista[3][1])
+print(lista[3][2])
+'''
+
+#tupla = ("odenador", "teclado", "raton")
+#print(tupla)
+#print(len(tupla))
+#print(tupla[0])
+#print(tupla[1])
+#print(tupla[2])
+
+#mesestraducidos = {"enero":"january",
+#                   "febrero":"february",
+#                   "marzo":"march",
+#                   "abril":"april",
+#                   "mayo":"may",
+#                   "junio":"june",
+#                   "julio":"july",
+#                   "agosto":"august",
+#                   "septiembre":"september",
+#                   "octubre":"october",
+#                   "noviembre":"november",
+#                   "diciembre":"december"}
+
+#print(mesestraducidos["noviembre"])
+#print(mesestraducidos["mayo"])
